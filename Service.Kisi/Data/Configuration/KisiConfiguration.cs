b68c149af8a428d0e644d98f932181dbeb302fab
omer.kosar.33 <omer.kosar.33@gmail.com>
2023-06-13 23:06:35 +0300

    Kişi entity seed data oluşturuldu

--- Service.Kisi/Data/Configuration/KisiConfiguration.cs
+++ Service.Kisi/Data/Configuration/KisiConfiguration.cs
@@ -16,6 +16,39 @@ namespace Service.Kisi.Data.Configuration
             builder.Property(e => e.Firma)
                .HasMaxLength(100)
                .IsRequired(true);
+
+            builder.HasData(new Entities.Kisi
+            {
+                Id = new Guid("856b39fb-d802-4574-a0b4-872a12589c59"),
+                Ad = "John",
+                Soyad = "Doe",
+                Firma = "ABC Corporation"
+
+            },
+          new Entities.Kisi
+          {
+              Id = new Guid("927f71b9-fef6-44ce-b6d8-a8ee86df89e0"),
+              Ad = "Jane",
+              Soyad = "Smith",
+              Firma = " XYZ Ltd."
+
+          },
+          new Entities.Kisi
+          {
+              Id = new Guid("a5ce0ec5-1c1a-4a15-86f9-cdde439a92a2"),
+              Ad = "Michael",
+              Soyad = "Johnson",
+              Firma = "QWERTY Holdings"
+
+          },
+          new Entities.Kisi
+          {
+              Id = new Guid("e0207c71-f788-4067-8ebf-3a48e7b6966e"),
+              Ad = "Emily",
+              Soyad = "Brown",
+              Firma = "XYZ Ltd."
+
+          });
         }
     }
 }
--- Service.Kisi/Migrations/20230613200609_KisiEntitySeedDataEklendi.Designer.cs
+++ Service.Kisi/Migrations/20230613200609_KisiEntitySeedDataEklendi.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using Service.Kisi.Data;
@@ -11,9 +12,11 @@ using Service.Kisi.Data;
 namespace Service.Kisi.Migrations
 {
     [DbContext(typeof(KisiContext))]
-    partial class KisiContextModelSnapshot : ModelSnapshot
+    [Migration("20230613200609_KisiEntitySeedDataEklendi")]
+    partial class KisiEntitySeedDataEklendi
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
