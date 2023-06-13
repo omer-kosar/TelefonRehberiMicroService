5ceecc201ba9c53535b727054e877b20ff6f6e83
omer.kosar.33 <omer.kosar.33@gmail.com>
2023-06-13 23:43:31 +0300

    api versiyonlama nuget paketleri eklendi

---
+++ Service.Kisi/Controllers/v1/PersonsController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Service.Kisi.Controllers.v1
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PersonsController : ControllerBase
+    {
+    }
+}
