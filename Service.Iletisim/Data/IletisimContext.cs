03af2406fdaa40f0e1c30e963b1db483a0d33af1
omer-kosar <omer.kosar.33@gmail.com>
2023-06-14 09:23:44 +0300

    iletişim context eklendi

---
+++ Service.Iletisim/Data/IletisimContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Service.Iletisim.Data
+{
+    public class IletisimContext : DbContext
+    {
+        public IletisimContext(DbContextOptions options) : base(options)
+        {
+
+        }
+        public DbSet<Entities.Iletisim> Iletisim { get; set; }
+    }
+}
