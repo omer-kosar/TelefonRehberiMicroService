b847d5d6f393fe062e6733295005cac6cef90717
omer-kosar <omer.kosar.33@gmail.com>
2023-06-14 16:51:40 +0300

    rabbitmq client nuget paketi eklendi, rapor talebini rabbitmq sunucusuna gönderecek rabbitmqprodcuer sınıfı eklendi

---
+++ TelefonRehberi.APIGateway/RabbitMQ/RabbitMQProducer.cs
@@ -0,0 +1,30 @@
+﻿using Newtonsoft.Json;
+using RabbitMQ.Client;
+using System.Text;
+
+namespace TelefonRehberi.APIGateway.RabbitMQ
+{
+    public class RabbitMQProducer : IMessageProducer
+    {
+        public void SendRaporOlusturMesaj<T>(T message)
+        {
+            var factory = new ConnectionFactory
+            {
+                HostName = "localhost"
+            };
+
+            var connection = factory.CreateConnection();
+            using var channel = connection.CreateModel();
+
+            channel.QueueDeclare("rapor-talepleri", durable: true,
+                            exclusive: false,
+                            autoDelete: false,
+                            arguments: null);
+
+            var json = JsonConvert.SerializeObject(message);
+            var body = Encoding.UTF8.GetBytes(json);
+
+            channel.BasicPublish(exchange: "", routingKey: "rapor-talepleri", body: body);
+        }
+    }
+}
