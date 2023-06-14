06d1ecfee04b6bb2e4164c0fdbc93ae072f22548
omer-kosar <omer.kosar.33@gmail.com>
2023-06-14 13:25:22 +0300

    raporcontext eklendi

---
+++ Service.Rapor/Data/RaporContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Service.Rapor.Entities;
+
+namespace Service.Kisi.Data
+{
+    public class RaporContext : DbContext
+    {
+        public RaporContext(DbContextOptions options) : base(options)
+        {
+
+        }
+        public DbSet<Rapor.Entities.Rapor> Rapor { get; set; }
+        public DbSet<Rapor.Entities.RaporBilgi> RaporBilgi { get; set; }
+
+    }
+}
