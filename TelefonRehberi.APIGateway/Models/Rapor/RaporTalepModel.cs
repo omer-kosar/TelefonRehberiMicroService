a0ad49232399014211749062851c36ca8c8c51dd
omer-kosar <omer.kosar.33@gmail.com>
2023-06-14 16:56:54 +0300

    raporkaydedildikten sonra rapor talebi rabbitmq sunucusuna gönderildi

---
+++ TelefonRehberi.APIGateway/Models/Rapor/RaporTalepModel.cs
@@ -0,0 +1,8 @@
+﻿namespace TelefonRehberi.APIGateway.Models.Rapor
+{
+    public class RaporTalepModel
+    {
+        public Guid RaporId { get; set; }
+        public string Konum { get; set; }
+    }
+}
