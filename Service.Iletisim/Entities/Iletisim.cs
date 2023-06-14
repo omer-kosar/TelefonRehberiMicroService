94a2f33ef296edea95d5121f4a54408b2817db85
omer-kosar <omer.kosar.33@gmail.com>
2023-06-14 09:19:24 +0300

    iletişim entity eklendi

---
+++ Service.Iletisim/Entities/Iletisim.cs
@@ -0,0 +1,10 @@
+﻿namespace Service.Iletisim.Entities
+{
+    public class Iletisim
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public Guid KisiId { get; set; }
+        public int IletisimType { get; set; }
+        public string Icerik { get; set; }
+    }
+}
