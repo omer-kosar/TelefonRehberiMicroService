289f2772b0bbcba0be7e59e249574054f4706773
omer-kosar <omer.kosar.33@gmail.com>
2023-06-14 13:20:30 +0300

    rapor ve raporbilgi entity sınıfları eklendi

---
+++ Service.Rapor/Entities/Rapor.cs
@@ -0,0 +1,10 @@
+﻿namespace Service.Rapor.Entities
+{
+    public class Rapor
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public DateTimeOffset TalepEdildigiTarih { get; set; }
+        public int Durum { get; set; }
+        public ICollection<RaporBilgi> RaporBilgileri { get; set; }
+    }
+}
