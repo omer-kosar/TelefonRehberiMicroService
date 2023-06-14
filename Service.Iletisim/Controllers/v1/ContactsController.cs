e70de1e094abfdbc0e4eb81513e70bb4ecb34135
omer-kosar <omer.kosar.33@gmail.com>
2023-06-14 11:34:28 +0300

    versiyonlama ayarları yapıldı iletişim endpointleri için contactscontroller v1 olarak eklendi

---
+++ Service.Iletisim/Controllers/v1/ContactsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Service.Iletisim.Controllers.v1
+{
+    [Route("api/v{version:apiVersion}/contacts")]
+    [ApiVersion("1.0")]
+    [ApiController]
+    public class ContactsController : ControllerBase
+    {
+    }
+}
--- Service.Iletisim/Extensions/ServiceRegistrationExtensions.cs
+++ Service.Iletisim/Extensions/ServiceRegistrationExtensions.cs
@@ -9,5 +9,18 @@ namespace Service.Iletisim.Extensions
         {
             services.AddDbContext<IletisimContext>(options => options.UseNpgsql(configuration.GetConnectionString("postgreSqlConnection")));
         }
+        public static void ConfigureVersioning(this IServiceCollection services) => services.AddApiVersioning(opt =>
+        {
+            opt.ReportApiVersions = true;
+            opt.AssumeDefaultVersionWhenUnspecified = true;
+            opt.DefaultApiVersion = new Microsoft.AspNetCore.Mvc.ApiVersion(1, 0);
+        });
+        public static void ConfigureVersionedApiExplorer(this IServiceCollection services) =>
+            services.AddVersionedApiExplorer(
+            options =>
+            {
+                options.GroupNameFormat = "'v'VVV";
+                options.SubstituteApiVersionInUrl = true;
+            });
     }
 }
