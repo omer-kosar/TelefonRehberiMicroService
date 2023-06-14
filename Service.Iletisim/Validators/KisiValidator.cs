e0ca4957d0342d2452edbb6e934b8f800128157b
omer-kosar <omer.kosar.33@gmail.com>
2023-06-14 12:12:17 +0300

    iletşim validator eklendi

---
+++ Service.Iletisim/Validators/KisiValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+using Service.Iletisim.Dto;
+
+namespace Service.Iletisim.Validators
+{
+    public class IletisimValidator : AbstractValidator<IletisimDto>
+    {
+        public IletisimValidator()
+        {
+            RuleFor(x => x.Icerik).NotEmpty().WithMessage("İletişim bilgisi boş olamaz.")
+                .MaximumLength(50).WithMessage("İletişim bilgisi 50 karakterden fazla olamaz.");
+        }
+    }
+}
