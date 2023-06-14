9b81bb8be49c09f07ac7b8c68e6db1f51f2f2e03
omer-kosar <omer.kosar.33@gmail.com>
2023-06-14 09:35:45 +0300

    iletişim entity başlangıç migration eklendi

---
+++ Service.Iletisim/Migrations/20230614063512_IletisimEntityOlusturma.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Service.Iletisim.Migrations
+{
+    /// <inheritdoc />
+    public partial class IletisimEntityOlusturma : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Iletisim",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    KisiId = table.Column<Guid>(type: "uuid", nullable: false),
+                    IletisimType = table.Column<int>(type: "integer", nullable: false),
+                    Icerik = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Iletisim", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Iletisim");
+        }
+    }
+}
