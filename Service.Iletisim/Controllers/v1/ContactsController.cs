11557d07ed20ffd4ac2fc859735132cb09ee6d01
omer-kosar <omer.kosar.33@gmail.com>
2023-06-14 11:51:13 +0300

    iletişim kaydeden kaydetiletisim endpoint ve test metodu eklendi

--- Service.Iletisim/Controllers/v1/ContactsController.cs
+++ Service.Iletisim/Controllers/v1/ContactsController.cs
@@ -1,6 +1,10 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Mapster;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Service.Iletisim.Dto;
+using Service.Iletisim.Entities;
 using Service.Iletisim.Repositories.Interfaces;
+using System.Net;
 
 namespace Service.Iletisim.Controllers.v1
 {
@@ -9,7 +13,6 @@ namespace Service.Iletisim.Controllers.v1
     [ApiController]
     public class ContactsController : ControllerBase
     {
-        //iletişim kaydet
         //iletişim sil
         //konum raporu getir
         //kişi id ile iletişim bilgileri getir
@@ -18,6 +21,15 @@ namespace Service.Iletisim.Controllers.v1
         {
             _iletisimRepository = iletisimRepository ?? throw new ArgumentNullException(nameof(iletisimRepository)); ;
         }
-        
+
+        [HttpPost]
+        [ProducesResponseType((int)HttpStatusCode.OK)]
+        public async Task<IActionResult> KaydetIletisim([FromBody] IletisimDto iletisim)
+        {
+            //todo:validation eklenecek
+            var iletisimEntity = iletisim.Adapt<Entities.Iletisim>();
+            await _iletisimRepository.IletisimKaydet(iletisimEntity);
+            return Ok(iletisimEntity.Id);
+        }
     }
 }
---
+++ UnitTest.Iletisim/ContactsControllerTests.cs
@@ -0,0 +1,105 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Service.Iletisim.Controllers.v1;
+using Service.Iletisim.Data;
+using Service.Iletisim.Dto;
+using Service.Iletisim.Enums;
+using Service.Iletisim.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTest.Iletisim
+{
+    public class ContactsControllerTests
+    {
+
+
+        private DbContextOptions<IletisimContext> dbContextOptions;
+        public ContactsControllerTests()
+        {
+            var dbName = $"IletisimDb_{DateTime.Now.ToFileTimeUtc()}";
+            dbContextOptions = new DbContextOptionsBuilder<IletisimContext>()
+                .UseInMemoryDatabase(dbName)
+                .Options;
+        }
+
+        [Fact]
+        public async Task GecerliIletisimModelIleIletisimKaydedildiginde_IletisimKaydeder()
+        {
+            var repository = await CreateRepositoryAsync();
+            var contactsController = new ContactsController(repository);
+            var iletisimDto = new IletisimDto
+            {
+                Id = Guid.NewGuid(),
+                KisiId = new Guid("856b39fb-d802-4574-a0b4-872a12589c59"),
+                IletisimType = (int)IletisimType.Telefon,
+                Icerik = "05383943344"
+            };
+            var result = await contactsController.KaydetIletisim(iletisimDto) as ObjectResult;
+
+            Assert.NotNull(result);
+            Assert.Equal(StatusCodes.Status200OK, result.StatusCode);
+            Assert.Equal(true, Guid.TryParse(result.Value.ToString(), out var iletisimId));
+        }
+        private async Task<IletisimRepository> CreateRepositoryAsync()
+        {
+            IletisimContext context = new IletisimContext(dbContextOptions);
+            await PopulateDataAsync(context);
+            return new IletisimRepository(context);
+        }
+        private async Task PopulateDataAsync(IletisimContext context)
+        {
+
+            await context.Iletisim.AddRangeAsync(new List<Service.Iletisim.Entities.Iletisim>
+                {
+                     new Service.Iletisim.Entities.Iletisim
+                    {
+                         Id=new Guid("e85e3cf0-974e-4bc5-9dee-7d47094d039e"),
+                         KisiId = new Guid("856b39fb-d802-4574-a0b4-872a12589c59"),
+                         IletisimType = (int)IletisimType.Telefon,
+                         Icerik = "05383941232"
+                    },
+                    new Service.Iletisim.Entities.Iletisim
+                    {
+                        Id = new Guid("6eb946fb-e1c4-4c02-a8c6-c4a9423b9fa5"),
+                        KisiId = new Guid("856b39fb-d802-4574-a0b4-872a12589c59"),
+                        IletisimType = (int)IletisimType.EPosta,
+                        Icerik = "abc@abc.com"
+                    },
+                    new Service.Iletisim.Entities.Iletisim
+                    {
+                       Id = new Guid("c042ae87-1a13-4cd2-a281-fa7f7b579118"),
+                       KisiId = new Guid("856b39fb-d802-4574-a0b4-872a12589c59"),
+                       IletisimType = (int)IletisimType.Konum,
+                       Icerik = "Ankara"
+                    },
+                    new Service.Iletisim.Entities.Iletisim
+                    {
+                          Id = new Guid("1f1dfa36-bbde-4fd3-87bd-4c0aaed711b1"),
+                          KisiId = new Guid("927f71b9-fef6-44ce-b6d8-a8ee86df89e0"),
+                          IletisimType = (int)IletisimType.Konum,
+                          Icerik = "Ankara"
+                    },new Service.Iletisim.Entities.Iletisim
+                    {
+                         Id = new Guid("550de627-2176-43bd-a40a-fce08ebb8465"),
+                          KisiId = new Guid("927f71b9-fef6-44ce-b6d8-a8ee86df89e0"),
+                          IletisimType = (int)IletisimType.Telefon,
+                          Icerik = "05383946677"
+                    }
+                    ,new Service.Iletisim.Entities.Iletisim
+                    {
+                        Id = new Guid("1cdac1b6-f64f-4dd2-91bd-475ff2812de6"),
+                          KisiId = new Guid("927f71b9-fef6-44ce-b6d8-a8ee86df89e0"),
+                          IletisimType = (int)IletisimType.Telefon,
+                          Icerik = "05383948899"
+                    }
+                });
+
+            await context.SaveChangesAsync();
+        }
+    }
+}
